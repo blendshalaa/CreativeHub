931d72fd740253ced7dc87b70c843cbc38fd3c1b
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2023-06-05 17:09:28 +0200

    firsts section html and css setup ready for js

---
+++ script.js
@@ -0,0 +1,11 @@
+"strict"
+document.querySelector('.button-circle').addEventListener('click',function(){
+console.log('You cliked it')
+});
+
+
+document.querySelector('.events-webinars').addEventListener('click',function(event){
+   event.target.style.backgroundColor='#243F60';
+})
+//improve this event 
+
